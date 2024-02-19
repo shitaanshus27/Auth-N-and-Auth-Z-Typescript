55ed891a57b89f63c8c50ea75f5b37b1c8098513
Shitaanshu Singh <122525450+shitaanshus27@users.noreply.github.com>
2024-02-19 15:57:55 +0530

    Add files via upload

--- Auth.ts
+++ Auth.ts
@@ -1,120 +1,44 @@
-import User from "../models/User";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import bcrypt from "bcryptjs";
-import { Response, Request } from "express";
-
+import { Response, Request, NextFunction } from "express";
 dotenv.config();
 
-export const signup = async (req: Request, res: Response) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      confirmPassword,
-      accountType,
-    } = req.body;
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader;
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+      if (err || !payload || typeof payload === "string") {
+        // If there's an error or the payload is invalid, send a 403 response
+        return res.sendStatus(403);
+      }
 
-    if (
-      !firstName ||
-      !lastName ||
-      !email ||
-      !password ||
-      !confirmPassword ||
-      !accountType
-    ) {
-      return res.status(403).send({
-        success: false,
-        message: "All Fields are required",
-      });
-    }
+      req.headers["userId"] = payload.id;
+      req.headers["accountType"] = payload.accountType;
+      req.headers["userEmail"] = payload.email;
 
-    if (password !== confirmPassword) {
-      return res.status(400).json({
-        success: false,
-        message:
-          "Password and Confirm Password do not match. Please try again.",
-      });
-    }
-
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({
-        success: false,
-        message: "User already exists. Please sign in to continue.",
-      });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-      accountType,
+      next();
     });
-
-    return res.status(200).json({
-      success: true,
-      user,
-      message: "User registered successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      success: false,
-      message: "User cannot be registered. Please try again.",
-    });
+  } else {
+    res.sendStatus(401);
   }
 };
 
-export const login = async (req: Request, res: Response) => {
+export const isStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const { email, password } = req.body;
+    const accountType = req.headers["accountType"];
 
-    console.log(email);
-    console.log(password);
-
-    if (!email || !password) {
-      return res.status(400).json({
+    if (accountType != "STUDENT") {
+      res.status(401).json({
         success: false,
-        message: `Please Fill up All the Required Fields`,
+        message: "Yeh Student ke liye hai sir",
       });
     }
-    const user = await User.findOne({ email });
-
-    console.log(user);
-
-    if (!user) {
-      return res.status(401).json({
-        success: false,
-        message: `User is not Registered with Us Please SignUp to Continue`,
-      });
-    }
-
-    if (await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign(
-        { email: user.email, id: user._id, accountType: user.accountType },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "24h",
-        }
-      );
-      return res.status(200).json({
-        success: true,
-        token,
-      });
-    } else {
-      return res.status(401).json({
-        success: false,
-        message: `Password is incorrect`,
-      });
-    }
+    next();
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -124,30 +48,26 @@ export const login = async (req: Request, res: Response) => {
   }
 };
 
-export const InstructorDash = async (req: Request, res: Response) => {
+export const isInstructor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    return res.status(200).json({
-      success: true,
-      message: "Welcome To Instuctor Section",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: "Something Went Wrong. Please try again.",
-    });
-  }
-};
+    const accountType = req.headers["accountType"];
 
-export const StudentDash = async (req: Request, res: Response) => {
-  try {
-    return res.status(200).json({
-      success: true,
-      message: "Welcome To Student Section",
-    });
+    if (accountType != "Instructor") {
+      res.status(401).json({
+        success: false,
+        message: "Bakchodi Mat Kar Sir hun main",
+      });
+    }
+    next();
   } catch (error) {
+    console.error(error);
     return res.status(500).json({
       success: false,
-      message: "Something Went Wrong. Please try again.",
+      message: `Login Failure Please Try Again`,
     });
   }
 };
